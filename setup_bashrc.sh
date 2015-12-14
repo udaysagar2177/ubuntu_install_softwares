cb9a1afe1a7a861a27a6d11bf3a6d2aae90fbdae
udaysagar2177 <udaysagar.2177@gmail.com>
2015-12-14 01:49:27 -0800

    Git aliases

---
+++ setup_bashrc.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Setup bashrc
+echo "# git aliases" >> ~/.bashrc
+echo "alias g=\"git\"" >> ~/.bashrc
+echo "alias gc=\"git commit\"" >> ~/.bashrc
+echo "alias gs=\"git status\"" >> ~/.bashrc
+echo "alias gl=\"git log\"" >> ~/.bashrc
+echo "alias gpush=\"git push origin\"" >> ~/.bashrc
+echo "alias gadd=\"git add --all\"" >> ~/.bashrc
