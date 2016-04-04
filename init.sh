63567962fe2aab1e4bc82ed8e37e8a67fb7d1291
udaysagar2177 <udaysagar.2177@gmail.com>
2016-04-04 14:26:10 -0700

    Update setup_bashrc.sh Remove install_docker_on_ubuntu.sh Install docker using official docker install script Update travis.yml

---
+++ init.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -xe
+
+#determine if the script is being run by root or not
+user=$(whoami)
+if [ "$user" == "root" ]; then
+	sudo=""
+else
+	sudo="sudo"
+fi
+
+# update system
+$sudo apt-get update
+
+# install basic softwares
+$sudo apt-get install -y vim curl wget net-tools python
+
+# Install and setup GIT
+$sudo apt-get install -y git
+git config --global user.name "udaysagar2177"
+git config --global user.email "udaysagar.2177@gmail.com"
+
+# install pip
+wget https://bootstrap.pypa.io/get-pip.py
+$sudo python get-pip.py
+rm get-pip.py
+
+# Do not execute below commands if script is being executed on Docker container
+if [[ $1 != "on_docker" ]]; then
+	# install docker
+	curl -sSL https://get.docker.com/ | $sudo sh
+
+	# install maestro-NG
+	$sudo pip install maestro
+fi
+
+# setup bashrc
+$sudo ./setup_bashrc.sh
--- setup_bashrc.sh
+++ setup_bashrc.sh
@@ -1,19 +1,45 @@
 #!/bin/bash
+set -xe
+
+# This script is called in init.sh
 
 # Setup bashrc
-echo "# git aliases" >> ~/.bashrc
-echo "alias g=\"git\"" >> ~/.bashrc
-echo "alias gc=\"git commit\"" >> ~/.bashrc
-echo "alias gs=\"git status\"" >> ~/.bashrc
-echo "alias gl=\"git log\"" >> ~/.bashrc
-echo "alias gpush=\"git push origin\"" >> ~/.bashrc
-echo "alias gadd=\"git add --all\"" >> ~/.bashrc
+cat << EOT > ~/.my_bashrc
+# git aliases
+alias g="git"
+alias gc="git commit"
+alias gs="git status"
+alias gl="git log"
+alias gpush="git push origin"
+alias gadd="git add --all"
+alias gpull="git pull origin"
+alias gd="git diff"
+
+# common
+alias ll="ls -alF"
+alias ps="ps -ef"
+
+# docker
+alias dp="docker ps"
+## kills docker none images
+alias killdn="docker images | grep none | awk '{print $3}' | xargs docker rmi"
+## launch a ubuntu container
+alias dubuntu="docker run -it ubuntu bash"
+## launch a centos container
+alias dcentos="docker run -it centos:6 bash"
+## exec into the container listed at position $1 in docker ps
+dex(){
+container=$(docker ps --format "{{.Names}}" | sed -n ${1}p)
+echo "Getting inside "$container
+docker exec -it $container bash
+}
+EOT
 
 # source the changes
-source ~/.bashrc
+source ~/.my_bashrc
 
 # make an entry in ~/.profile to source ~/.bashrc 
 # for every new terminal
-echo "if [ -s ~/.bashrc ]; then" >> ~/.profile
-echo "    source ~/.bashrc;" >> ~/.profile
+echo "if [ -s ~/.my_bashrc ]; then" >> ~/.profile
+echo "    source ~/.my_bashrc;" >> ~/.profile
 echo "fi" >> ~/.profile
